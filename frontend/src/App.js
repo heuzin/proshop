8780896ed009bd0fd4bb899b29b16dece68d1591
heuzin <matheusqs.arq@gmail.com>
2021-04-26 11:18:12 -0400

    header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome To ProShop</h1>
-    </div>
+      <div>
+        <Header />
+        <main>
+          <h1>Welcome To ProShop</h1>
+        </main>
+        <Footer />
+      </div>
   );
 }
 
