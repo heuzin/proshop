feb4de63effdd4493f8f3b62cfbf94c435d2f0e9
heuzin <matheusqs.arq@gmail.com>
2021-04-28 21:22:55 -0400

    data seeder script

--- backend/models/orderModal.js
+++ backend/models/orderModal.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema({
     user: {
@@ -64,7 +64,7 @@ const orderSchema = mongoose.Schema({
         default: false
     },
     deliveredAt: {
-        type: Data,
+        type: Date,
     },
 }, {
     timestamps: true
