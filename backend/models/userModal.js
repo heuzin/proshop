a9257a819f1815d4424a4e7da173366ce89db4b9
heuzin <matheusqs.arq@gmail.com>
2021-05-01 21:40:14 -0400

    user authentication endpoint

--- backend/models/userModal.js
+++ backend/models/userModal.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -20,8 +21,13 @@ const userSchema = mongoose.Schema({
         default: false
     }
 }, {
-    timestamps: true
-})
+    timestamps: true    
+    }
+)
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
