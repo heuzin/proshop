3990c067fb68167ddfc363235cefbe97b41687e7
heuzin <matheusqs.arq@gmail.com>
2021-05-06 16:33:06 -0400

    update Order to paid:

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { json } from 'express'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModal.js'
 
@@ -51,4 +52,29 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems, getOrderById }
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Public
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if(order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: res.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res,json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
